4928bdbadd02021e7d55cb917a6b23bbcdb9ff4a
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-04-11 10:58:39 +0100

    basic linux admin tasks

---
+++ 04-basic administration task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: Salaudeen O. Abdulrasaq
+# Date: 04/11/2022
+# Date Modified: 04/11/2022
+echo
+echo "This script will run basic administrative commands"
+echo
+top | head -10
+echo
+df -h
+echo
+free -m 
+echo
+uptime
+echo
+iostat
+echo
+echo End of Script
