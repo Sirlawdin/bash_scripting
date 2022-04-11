0ed1df90f56c4fc12303bb00898f7bfb64dcd746
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-04-11 10:44:08 +0100

    Created a script that runs some basic linux tasks

---
+++ 03-run basic linux tasks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Author: Salaudeen O. Abdulrasaq
+# Date Created: 04/11/2022
+# Description: Run basic linux tasks
+# Date Modified: 04/11/2022
+
+echo
+echo 'This script will run a few basic tasks in linux'
+echo
+pwd
+echo
+ls
+echo
+whoami
+echo
+date
+echo
+cal
+echo
+touch file1 file2 file3
+echo End of script
