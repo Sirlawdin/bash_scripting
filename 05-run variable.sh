ddd4324957d111dde147b2e2fa182cce74c01992
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-04-11 11:06:38 +0100

    Defining Variables

---
+++ 05-run variable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Author: Salaudeen O. Abdulrasaq
+# Date Created: 04/11/2022 (MM/DD/YY)
+# Description: Defining Variables
+# Date Modified: 04/11/2022
+
+
+# Define Variables
+p=pwd
+l=ls
+wi=whoami
+d=date
+c='cal 2022'
+
+
+echo Run variables tasks
+echo
+$p
+$l
+$wi
+$d
+$c
